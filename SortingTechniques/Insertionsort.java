b78762b2a1c09a48ffb4180ee75f1dac8830245a
Vedika Pawar <vedikapawar051@gmail.com>
2025-10-24 19:43:28 +0530

    Adding Insertion sort(Increasing order)

---
+++ SortingTechniques/Insertionsort.java
@@ -0,0 +1,31 @@
+public class Insertionsort {
+    public static void insertionsort(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+            // Compare previous element with current element
+            while (prev >= 0 && arr[prev] > curr) {
+                // Finding correct Position for current element to insert.
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            // Insertion of Current Element
+            arr[prev + 1] = curr;
+        }
+
+    }
+
+    public static void printarr(int arr[]) {
+        System.out.println("Sorted Array using Insertion Sort:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 1, 3, 2 };
+        insertionsort(arr);
+        printarr(arr);
+    }
+}
